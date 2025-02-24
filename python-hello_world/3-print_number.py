f8d8f4396f8a868b38e1194a3f8841c703e73522
Ptuyizere <p.tuyizere1@alustudent.com>
2025-02-24 14:32:18 +0200

    Added 3-print_number.py

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-num = 98
-print(f"{int(num):d} Battery street") if isinstance(num, (int, float)) else print(f"{num:d} Bettery street")
+number = 98
+print(f"{int(number):d} Battery street") if isinstance(num, (int, float)) else print(f"{number:d} Bettery street")
